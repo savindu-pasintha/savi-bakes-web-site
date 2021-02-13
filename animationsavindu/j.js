5d06bb247a033ad4f5e85f651fd0e56e62746350
savindu-pasintha <savindupasingtha@gmail.com>
2021-02-13 22:32:51 +0530

    savi bakes web site 2020-7-1

---
+++ animationsavindu/j.js
@@ -0,0 +1,40 @@
+// JavaScript 
+var x=function{
+	ellipse(mouseX mouseY 20 20);
+};
+
+// JavaScript jquery code Document 
+
+/*footer add
+ $(function(){
+	$(".footerpagehome").load("footer/footerhtml.html");
+ });
+
+$(function(){
+	$(".footerpageabout").load("../footer/footerhtml.html");
+});
+
+$(function(){
+	$(".footerpagecontact").load("../footer/footerhtml.html");
+});
+
+$(function(){
+	$(".footerpagegalary").load("../footer/footerhtml.html");
+});
+*/
+
+//header nav bar add
+
+/*
+$(function(){
+	$(".headergalary").load("../nav/navbar.html");
+	 });
+
+$(function(){
+	$(".headerabout").load("../nav/navbar.html");
+	 });
+
+$(function(){
+	$(".headercontact").load("../nav/navbar.html");
+	 });
+*/
---
+++ jquaryfile/savindu jquery coding file .js
@@ -0,0 +1,22 @@
+
+$(document).ready(function(){
+    
+	
+	$(".footerpagegalary").load("../footer/footerhtml.html");
+	$(".footerpageabout").load("../footer/footerhtml.html");
+	$(".footerpagecontact").load("../footer/footerhtml.html")
+	;
+	
+	$(".footerpagegalary").load("../footer/footerhtml.html");
+	
+	$(".headerhome").load("../nav/barnavbar.html");
+	
+	$(".headergalary").load("../barnav/navbar.html");
+	
+	$(".headerabout").load("../barnav/navbar.html");
+	
+	$(".headercontact").load("../barnav/navbar.html");
+	
+	});
+
+//jquery using load footer and navigation bar 
